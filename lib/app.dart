e7dad422146872f52a67b3ad36e4a1d5f02634f1
Noella Mwanri <nuwymwanri@gmail.com>
2020-08-26 01:06:39 +0300

    We created the buttons also  created list title which hade title and subtitle in part 3

--- lib/app.dart
+++ lib/app.dart
@@ -4,17 +4,42 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          color:  Colors.redAccent
-        )
-      ),
+      theme: ThemeData(appBarTheme: AppBarTheme(color: Colors.redAccent)),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Profile User'),
-        ),
-        body: Image.asset("images/us.jpg", width: 200, height: 100,),
-      ),
+          appBar: AppBar(
+            title: Text('Profile User'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Image.asset(
+                "images/us.jpg",
+                width: 200,
+                height: 100,
+              ),
+              ListTile(
+                title: Text("Joella Studio"),
+                subtitle: Text("Web developers and Graphic Designers"),
+              ),
+              RaisedButton(
+                onPressed: (){
+
+              },
+              child: Text("Message"),
+              ),
+
+               RaisedButton(
+                onPressed: (){
+
+              },
+              child: Text("Hire Me"),
+              ),
+
+               ListTile(
+                title: Text("About Us"),
+                subtitle: Text("Over 5 years experience on Web developement and Graphic Designers. We make different using flutter, React both front end and backend. Our experience with Graphic designers we make different logos, business cards, birthday cards also wedding cards and make flyers and posters. We give out the best service and we are remotely workers"),
+              ),
+            ],
+          )),
     );
   }
 }
